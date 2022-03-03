c7390f6b5d6183baeb6af8ce90e8b95be1d0163f
an0ni <an0ni@>
2022-03-03 18:52:59 +0300

    Добавьте файлы проекта.

---
+++ [C-Sharp] Music/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace _C_Sharp__Music
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Songs = new System.Windows.Forms.ListBox();
+            this.stop_button = new System.Windows.Forms.Button();
+            this.next_button = new System.Windows.Forms.Button();
+            this.previous_button = new System.Windows.Forms.Button();
+            this.playPause_button = new System.Windows.Forms.Button();
+            this.import_button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Songs
+            // 
+            this.Songs.FormattingEnabled = true;
+            this.Songs.Location = new System.Drawing.Point(9, 58);
+            this.Songs.Name = "Songs";
+            this.Songs.Size = new System.Drawing.Size(212, 160);
+            this.Songs.TabIndex = 0;
+            this.Songs.SelectedIndexChanged += new System.EventHandler(this.Songs_SelectedIndexChanged);
+            // 
+            // stop_button
+            // 
+            this.stop_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.stop_button;
+            this.stop_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.stop_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.stop_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stop_button.ForeColor = System.Drawing.SystemColors.Control;
+            this.stop_button.Location = new System.Drawing.Point(96, 11);
+            this.stop_button.Name = "stop_button";
+            this.stop_button.Size = new System.Drawing.Size(38, 41);
+            this.stop_button.TabIndex = 6;
+            this.stop_button.UseVisualStyleBackColor = true;
+            this.stop_button.Click += new System.EventHandler(this.stop_button_Click);
+            this.stop_button.MouseDown += new System.Windows.Forms.MouseEventHandler(this.stop_button_MouseDown);
+            this.stop_button.MouseUp += new System.Windows.Forms.MouseEventHandler(this.stop_button_MouseUp);
+            // 
+            // next_button
+            // 
+            this.next_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.next_button;
+            this.next_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.next_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.next_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.next_button.ForeColor = System.Drawing.SystemColors.Control;
+            this.next_button.Location = new System.Drawing.Point(140, 11);
+            this.next_button.Name = "next_button";
+            this.next_button.Size = new System.Drawing.Size(38, 41);
+            this.next_button.TabIndex = 5;
+            this.next_button.UseVisualStyleBackColor = true;
+            this.next_button.Click += new System.EventHandler(this.next_button_Click);
+            this.next_button.MouseDown += new System.Windows.Forms.MouseEventHandler(this.next_button_MouseDown);
+            this.next_button.MouseUp += new System.Windows.Forms.MouseEventHandler(this.next_button_MouseUp);
+            // 
+            // previous_button
+            // 
+            this.previous_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.prew_button;
+            this.previous_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.previous_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.previous_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.previous_button.ForeColor = System.Drawing.SystemColors.Control;
+            this.previous_button.Location = new System.Drawing.Point(9, 11);
+            this.previous_button.Name = "previous_button";
+            this.previous_button.Size = new System.Drawing.Size(38, 41);
+            this.previous_button.TabIndex = 4;
+            this.previous_button.UseVisualStyleBackColor = true;
+            this.previous_button.Click += new System.EventHandler(this.previous_button_Click);
+            this.previous_button.MouseDown += new System.Windows.Forms.MouseEventHandler(this.previous_button_MouseDown);
+            this.previous_button.MouseUp += new System.Windows.Forms.MouseEventHandler(this.previous_button_MouseUp);
+            // 
+            // playPause_button
+            // 
+            this.playPause_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.play_button;
+            this.playPause_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.playPause_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.playPause_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playPause_button.ForeColor = System.Drawing.SystemColors.Control;
+            this.playPause_button.Location = new System.Drawing.Point(52, 11);
+            this.playPause_button.Name = "playPause_button";
+            this.playPause_button.Size = new System.Drawing.Size(38, 41);
+            this.playPause_button.TabIndex = 2;
+            this.playPause_button.UseVisualStyleBackColor = true;
+            this.playPause_button.Click += new System.EventHandler(this.playPause_button_Click);
+            this.playPause_button.MouseDown += new System.Windows.Forms.MouseEventHandler(this.playPause_button_MouseDown);
+            this.playPause_button.MouseUp += new System.Windows.Forms.MouseEventHandler(this.playPause_button_MouseUp);
+            // 
+            // import_button
+            // 
+            this.import_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.import_button;
+            this.import_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.import_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.import_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.import_button.ForeColor = System.Drawing.SystemColors.Control;
+            this.import_button.Location = new System.Drawing.Point(183, 11);
+            this.import_button.Name = "import_button";
+            this.import_button.Size = new System.Drawing.Size(38, 41);
+            this.import_button.TabIndex = 1;
+            this.import_button.UseVisualStyleBackColor = true;
+            this.import_button.Click += new System.EventHandler(this.import_button_Click);
+            this.import_button.MouseDown += new System.Windows.Forms.MouseEventHandler(this.import_button_MouseDown);
+            this.import_button.MouseUp += new System.Windows.Forms.MouseEventHandler(this.import_button_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(140, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 264);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.stop_button);
+            this.Controls.Add(this.next_button);
+            this.Controls.Add(this.previous_button);
+            this.Controls.Add(this.playPause_button);
+            this.Controls.Add(this.import_button);
+            this.Controls.Add(this.Songs);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(249, 307);
+            this.MinimumSize = new System.Drawing.Size(249, 307);
+            this.Name = "Form1";
+            this.Text = "SUPER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Songs;
+        private System.Windows.Forms.Button import_button;
+        private System.Windows.Forms.Button playPause_button;
+        private System.Windows.Forms.Button next_button;
+        private System.Windows.Forms.Button previous_button;
+        private System.Windows.Forms.Button stop_button;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ [C-Sharp] Music/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _C_Sharp__Music
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        { InitializeComponent(); }
+
+        String[] files, paths; //пути и папки.
+        Boolean play_F = true; //флаг корректного отображения и работы кнопки play/pause
+
+        WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer(); // библиотека Windows.MediaPlayer.dll
+
+        //1. Выбор музыки 
+        private void import_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Select_Music = new OpenFileDialog();
+            Select_Music.Multiselect = true; //true - возможность выбрать несколько файлов
+            Select_Music.Filter = "Music |*.mp3;*.wav|All files |*.*"; //фильтры допустимых форматов
+            Select_Music.InitialDirectory = "C:\\Users\\Public\\Music"; //директория по-умолчанию
+
+            if (Select_Music.ShowDialog() == DialogResult.OK)
+                try
+                {
+                    files = Select_Music.SafeFileNames; //сохранение названий папки и музыки
+                    paths = Select_Music.FileNames; //путь к музыке (разница в том, что один с Safe, другой - без)
+                    for (int i = 0; i < files.Length; i++) //отображение музыки или плейлиста
+                        Songs.Items.Add(files[i]);
+                }
+                catch
+                {
+                    //ну мало-ли, кто додумается *.jpeg открыть
+                    MessageBox.Show("File open error", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+        }
+
+        //2. Проигрывать (приостановить) музыку, через указатели в ListBox
+        private void playPause_button_Click(object sender, EventArgs e)
+        {
+            if (Songs.Items.Count < 1)
+                MessageBox.Show("No music in playlist", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                //если со старта программы не было выбрано ни одного трека
+                //воспроизведение можно начать с начала списка кнопкой play
+                if (Songs.SelectedIndex < 0)
+                {
+                    player.URL = paths[0];
+                    Songs.SelectedIndex = 0;
+                }
+
+                if (play_F)
+                {
+                    player.controls.play();
+                    playPause_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.pause_button;
+                }
+                if (!play_F)
+                {
+                    player.controls.pause();
+                    playPause_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.play_button;
+                }
+                play_F = !play_F;
+            }
+        }
+
+        //3. Полностью остановить музыку 
+        private void stop_button_Click(object sender, EventArgs e)
+        {
+            if (Songs.Items.Count < 1)
+                MessageBox.Show("No music in playlist", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                player.controls.stop();
+
+                //анимация и корректная работа кнопки play (далее такие-же записи аналогично)
+                if (!play_F)
+                {
+                    playPause_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.play_button;
+                    play_F = !play_F;
+                }
+            }
+        }
+
+        //4. Выбрать предыдущий трек
+        private void previous_button_Click(object sender, EventArgs e)
+        {
+            if (Songs.Items.Count < 1)
+                MessageBox.Show("No music in playlist", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (Songs.SelectedIndex > 0)
+            {
+                player.URL = paths[Songs.SelectedIndex - 1];
+                Songs.SelectedIndex = Songs.SelectedIndex - 1;
+
+                if (play_F)
+                {
+                    playPause_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.pause_button;
+                    play_F = !play_F;
+                }
+            }
+        }
+
+        //5. Выбрать следующий трек
+        private void next_button_Click(object sender, EventArgs e)
+        {
+            if (Songs.Items.Count < 1)
+                MessageBox.Show("No music in playlist", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (Songs.SelectedIndex < files.Length - 1)
+            {
+                player.URL = paths[Songs.SelectedIndex + 1];
+                Songs.SelectedIndex = Songs.SelectedIndex + 1;
+                if (play_F)
+                {
+                    playPause_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.pause_button;
+                    play_F = !play_F;
+                }
+            }
+        }
+        private void Songs_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+            player.URL = paths[Songs.SelectedIndex];
+        }
+
+        //Обработчики анимации кнопок
+        private void playPause_button_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (play_F)
+                playPause_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.play_button_pressed;
+            if (!play_F)
+                playPause_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.pause_button_pressed;
+        }
+        private void playPause_button_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (play_F)
+                playPause_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.play_button;
+            if (!play_F)
+                playPause_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.pause_button;
+        }
+
+        private void previous_button_MouseDown(object sender, MouseEventArgs e)
+        {
+            previous_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.prew_button_pressed;
+        }
+        private void previous_button_MouseUp(object sender, MouseEventArgs e)
+        {
+            previous_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.prew_button;
+        }
+
+        private void stop_button_MouseDown(object sender, MouseEventArgs e)
+        {
+            stop_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.stop_button_pressed;
+        }
+        private void stop_button_MouseUp(object sender, MouseEventArgs e)
+        {
+            stop_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.stop_button;
+        }
+
+        private void next_button_MouseDown(object sender, MouseEventArgs e)
+        {
+            next_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.next_button_pressed;
+        }
+        private void next_button_MouseUp(object sender, MouseEventArgs e)
+        {
+            next_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.next_button;
+        }
+
+        private void import_button_MouseDown(object sender, MouseEventArgs e)
+        {
+            import_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.import_button_pressed;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            player.settings.volume = player.settings.volume + 10;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) //в начале будет 
+        {
+            player.settings.volume = 50;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player.settings.volume = player.settings.volume - 10;
+        }
+
+        private void import_button_MouseUp(object sender, MouseEventArgs e)
+        {
+            import_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.import_button;
+        }
+    }
+}
+
+/*
+   ####### #######    ##   #######  ###   ##   ######     ######
+   ##      ###  ###   ##   ###      ###   ##   ##   ###  ###  ###
+   ##      ###   ##   ##   ###      ####  ##   ##   ###  ###
+   ##      ###   ##   ##   ###      ## #  ##   ##   ###   ###
+   ######  ########   ##   ######   ## ## ##   ##   ###    ####
+   ##      #######    ##   ###      ##  ####   ##   ###      ###
+   ##      ###  ##    ##   ###      ##  ####   ##   ###       ###  
+   ##      ###   ##   ##   ###      ##   ###   ##   ###  ###  ### 
+   ##      ###   ##   ##   #######  ##.  ###   ######     ######
+  
+                                              ``                             
+                              `.              `ys                            
+                              +h+             +yh-                           
+                              yyh:           .hyys                           
+                             .hyyh.          oyyyh`                          
+                             /yyyyy`        .hyydy/                          
+                             syyhhy+        oyyhsys                          
+                             hyyyoyh.      .hyyy:hh`                         
+                            .hyyyy:ho      +yyys-yh-                         
+                            :hyyyh-oh.    `hyyyo-oy/                         
+                            /yyyyh-:h+    -hyyh/-oy+                         
+                            +yyyyh:-yy    +yyyh--oyo                         
+                            +yyyyh/-sh.   syyyh--oyo                         
+                            +yyyyh/-oy/  `hyyyy--syo                         
+                            +yyyyh/-+y+  `hyyys--yy+                         
+                            :yyyyh/-+ys  .hyyyo-:hy:                         
+                            .hyyyh+-+ys  .hyyyo-oyh`                         
+                            `yyyyyo-oyy  .hyyy+-yyy                          
+                             +yyyys-syy  `hyyh/oyy/                          
+                             .hyyyh-hyy  `hyyh/hyh`                          
+                              oyyyhshys   yyyhyyy+                           
+                              oyyyhshys   yyyhyyy+                           
+                               /hyyyyyo`.-oyyyyh/                            
+                               `syyyyyyyhyyyyyyho.                           
+                                .hyyyyhNdyyyyyyymh/`                          
+        
+  
+
+*/
