e002c4ba3926c870d603f0395d7d27f7fb0af089
Alexey K <100595003+MeToDucT@users.noreply.github.com>
2022-03-03 19:21:50 +0300

    Update Form1.Designer.cs

--- [C-Sharp] Music/Form1.Designer.cs
+++ [C-Sharp] Music/Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace _C_Sharp__Music
+namespace _C_Sharp__Music
 {
-    partial class Form1
+    partial class PlayerWindow
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,25 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlayerWindow));
             this.Songs = new System.Windows.Forms.ListBox();
+            this.volumeDown_button = new System.Windows.Forms.Button();
+            this.volumeUp_button = new System.Windows.Forms.Button();
             this.stop_button = new System.Windows.Forms.Button();
             this.next_button = new System.Windows.Forms.Button();
             this.previous_button = new System.Windows.Forms.Button();
             this.playPause_button = new System.Windows.Forms.Button();
             this.import_button = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Songs
             // 
             this.Songs.FormattingEnabled = true;
-            this.Songs.Location = new System.Drawing.Point(9, 58);
+            this.Songs.ItemHeight = 16;
+            this.Songs.Location = new System.Drawing.Point(12, 71);
+            this.Songs.Margin = new System.Windows.Forms.Padding(4);
             this.Songs.Name = "Songs";
-            this.Songs.Size = new System.Drawing.Size(212, 160);
+            this.Songs.Size = new System.Drawing.Size(314, 244);
             this.Songs.TabIndex = 0;
             this.Songs.SelectedIndexChanged += new System.EventHandler(this.Songs_SelectedIndexChanged);
+            // 
+            // volumeDown_button
+            // 
+            this.volumeDown_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.volumeDown_button;
+            this.volumeDown_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.volumeDown_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.volumeDown_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.volumeDown_button.ForeColor = System.Drawing.SystemColors.Control;
+            this.volumeDown_button.Location = new System.Drawing.Point(301, 39);
+            this.volumeDown_button.Name = "volumeDown_button";
+            this.volumeDown_button.Size = new System.Drawing.Size(25, 25);
+            this.volumeDown_button.TabIndex = 8;
+            this.volumeDown_button.UseVisualStyleBackColor = true;
+            this.volumeDown_button.Click += new System.EventHandler(this.volumeDown_button_Click);
+            this.volumeDown_button.MouseDown += new System.Windows.Forms.MouseEventHandler(this.volumeDown_button_MouseDown);
+            this.volumeDown_button.MouseUp += new System.Windows.Forms.MouseEventHandler(this.volumeDown_button_MouseUp);
+            // 
+            // volumeUp_button
+            // 
+            this.volumeUp_button.BackgroundImage = global::_C_Sharp__Music.Properties.Resources.volumeUp_button;
+            this.volumeUp_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.volumeUp_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.volumeUp_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.volumeUp_button.ForeColor = System.Drawing.SystemColors.Control;
+            this.volumeUp_button.Location = new System.Drawing.Point(301, 13);
+            this.volumeUp_button.Name = "volumeUp_button";
+            this.volumeUp_button.Size = new System.Drawing.Size(25, 25);
+            this.volumeUp_button.TabIndex = 7;
+            this.volumeUp_button.UseVisualStyleBackColor = true;
+            this.volumeUp_button.Click += new System.EventHandler(this.volumeUp_button_Click);
+            this.volumeUp_button.MouseDown += new System.Windows.Forms.MouseEventHandler(this.volumeUp_button_MouseDown);
+            this.volumeUp_button.MouseUp += new System.Windows.Forms.MouseEventHandler(this.volumeUp_button_MouseUp);
             // 
             // stop_button
             // 
@@ -55,9 +89,10 @@
             this.stop_button.Cursor = System.Windows.Forms.Cursors.Hand;
             this.stop_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.stop_button.ForeColor = System.Drawing.SystemColors.Control;
-            this.stop_button.Location = new System.Drawing.Point(96, 11);
+            this.stop_button.Location = new System.Drawing.Point(128, 13);
+            this.stop_button.Margin = new System.Windows.Forms.Padding(4);
             this.stop_button.Name = "stop_button";
-            this.stop_button.Size = new System.Drawing.Size(38, 41);
+            this.stop_button.Size = new System.Drawing.Size(50, 50);
             this.stop_button.TabIndex = 6;
             this.stop_button.UseVisualStyleBackColor = true;
             this.stop_button.Click += new System.EventHandler(this.stop_button_Click);
@@ -71,9 +106,10 @@
             this.next_button.Cursor = System.Windows.Forms.Cursors.Hand;
             this.next_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.next_button.ForeColor = System.Drawing.SystemColors.Control;
-            this.next_button.Location = new System.Drawing.Point(140, 11);
+            this.next_button.Location = new System.Drawing.Point(186, 13);
+            this.next_button.Margin = new System.Windows.Forms.Padding(4);
             this.next_button.Name = "next_button";
-            this.next_button.Size = new System.Drawing.Size(38, 41);
+            this.next_button.Size = new System.Drawing.Size(50, 50);
             this.next_button.TabIndex = 5;
             this.next_button.UseVisualStyleBackColor = true;
             this.next_button.Click += new System.EventHandler(this.next_button_Click);
@@ -87,9 +123,10 @@
             this.previous_button.Cursor = System.Windows.Forms.Cursors.Hand;
             this.previous_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.previous_button.ForeColor = System.Drawing.SystemColors.Control;
-            this.previous_button.Location = new System.Drawing.Point(9, 11);
+            this.previous_button.Location = new System.Drawing.Point(12, 13);
+            this.previous_button.Margin = new System.Windows.Forms.Padding(4);
             this.previous_button.Name = "previous_button";
-            this.previous_button.Size = new System.Drawing.Size(38, 41);
+            this.previous_button.Size = new System.Drawing.Size(50, 50);
             this.previous_button.TabIndex = 4;
             this.previous_button.UseVisualStyleBackColor = true;
             this.previous_button.Click += new System.EventHandler(this.previous_button_Click);
@@ -103,9 +140,10 @@
             this.playPause_button.Cursor = System.Windows.Forms.Cursors.Hand;
             this.playPause_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.playPause_button.ForeColor = System.Drawing.SystemColors.Control;
-            this.playPause_button.Location = new System.Drawing.Point(52, 11);
+            this.playPause_button.Location = new System.Drawing.Point(70, 13);
+            this.playPause_button.Margin = new System.Windows.Forms.Padding(4);
             this.playPause_button.Name = "playPause_button";
-            this.playPause_button.Size = new System.Drawing.Size(38, 41);
+            this.playPause_button.Size = new System.Drawing.Size(50, 50);
             this.playPause_button.TabIndex = 2;
             this.playPause_button.UseVisualStyleBackColor = true;
             this.playPause_button.Click += new System.EventHandler(this.playPause_button_Click);
@@ -119,42 +157,23 @@
             this.import_button.Cursor = System.Windows.Forms.Cursors.Hand;
             this.import_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.import_button.ForeColor = System.Drawing.SystemColors.Control;
-            this.import_button.Location = new System.Drawing.Point(183, 11);
+            this.import_button.Location = new System.Drawing.Point(244, 13);
+            this.import_button.Margin = new System.Windows.Forms.Padding(4);
             this.import_button.Name = "import_button";
-            this.import_button.Size = new System.Drawing.Size(38, 41);
+            this.import_button.Size = new System.Drawing.Size(50, 50);
             this.import_button.TabIndex = 1;
             this.import_button.UseVisualStyleBackColor = true;
             this.import_button.Click += new System.EventHandler(this.import_button_Click);
             this.import_button.MouseDown += new System.Windows.Forms.MouseEventHandler(this.import_button_MouseDown);
             this.import_button.MouseUp += new System.Windows.Forms.MouseEventHandler(this.import_button_MouseUp);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(9, 229);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "+";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(140, 229);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "-";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(229, 264);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(334, 325);
+            this.Controls.Add(this.volumeDown_button);
+            this.Controls.Add(this.volumeUp_button);
             this.Controls.Add(this.stop_button);
             this.Controls.Add(this.next_button);
             this.Controls.Add(this.previous_button);
@@ -163,11 +182,10 @@
             this.Controls.Add(this.Songs);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(249, 307);
-            this.MinimumSize = new System.Drawing.Size(249, 307);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(356, 376);
             this.Name = "Form1";
             this.Text = "SUPER";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -180,8 +198,7 @@
         private System.Windows.Forms.Button next_button;
         private System.Windows.Forms.Button previous_button;
         private System.Windows.Forms.Button stop_button;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button volumeUp_button;
+        private System.Windows.Forms.Button volumeDown_button;
     }
 }
-
